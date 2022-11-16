24e630977f78865392860244c06cdcc66c281302
Pavel Popov <pcveykos@gmail.com>
2022-11-16 10:55:22 +0100

    Final linted version

--- frontend/src/pages/CupcakeDetails.jsx
+++ frontend/src/pages/CupcakeDetails.jsx
@@ -1,20 +1,20 @@
 import { useParams, Link } from "react-router-dom";
 import Cupcake from "@components/Cupcake";
 import { useEffect, useState } from "react";
+
 export default function CupcakeDetails() {
-  const {id} = useParams();
-  const [cupcake, setCupcake] = useState({})
+  const { id } = useParams();
+  const [cupcake, setCupcake] = useState({});
   useEffect(async () => {
-    const response = await fetch(`http://localhost:4000/cupcakes/${id}`)
-    const data = await response.json()
-    setCupcake(data)
-  }, [id])
-  console.log(cupcake)
+    const response = await fetch(`http://localhost:4000/cupcakes/${id}`);
+    const data = await response.json();
+    setCupcake(data);
+  }, [id]);
+
   return (
     <>
-
-    <Cupcake cupcake={cupcake} />
-    <Link to={'/cupcakes'} > Back to list</Link>
+      <Cupcake cupcake={cupcake} />
+      <Link to="/cupcakes"> Back to list</Link>
     </>
-  )
+  );
 }
--- frontend/src/pages/CupcakeList.jsx
+++ frontend/src/pages/CupcakeList.jsx
@@ -2,24 +2,21 @@
 import Cupcake from "@components/Cupcake";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+
 export default function CupcakeList() {
   // Step 1: get all cupcakes
-  const [cupcakes,setCupcakes] = useState([])
-  const [bonuses, setBonuses] = useState([])
-  const [filter, setFilter] = useState("")
-  useEffect(async() => {
-    try {
-      const response = await fetch('http://localhost:4000/cupcakes')
-      const data = await response.json()
-      const bonusResponse = await fetch('http://localhost:4000/accessories');
-      const bonusData = await bonusResponse.json()
-      setCupcakes(data)
-      setBonuses(bonusData)
-    } catch (error) {
-      console.log(error)
-    }
-  }, [])
-  if (cupcakes.length===0 || bonuses.length ===0) return 'Loading...'
+  const [cupcakes, setCupcakes] = useState([]);
+  const [bonuses, setBonuses] = useState([]);
+  const [filter, setFilter] = useState("");
+  useEffect(async () => {
+    const response = await fetch("http://localhost:4000/cupcakes");
+    const data = await response.json();
+    const bonusResponse = await fetch("http://localhost:4000/accessories");
+    const bonusData = await bonusResponse.json();
+    setCupcakes(data);
+    setBonuses(bonusData);
+  }, []);
+  if (cupcakes.length === 0 || bonuses.length === 0) return "Loading...";
   // Step 3: get all accessories
   return (
     <>
@@ -27,12 +24,17 @@ export default function CupcakeList() {
       <form className="center">
         <label htmlFor="cupcake-select">
           Filter by{" "}
-          <select onChange={e => setFilter(e.target.value)}  id="cupcake-select">
+          <select
+            onChange={(e) => setFilter(e.target.value)}
+            id="cupcake-select"
+          >
             <option value="">---</option>
-            {bonuses.map(bonus => {
+            {bonuses.map((bonus) => {
               return (
-                <option key={bonus.id} value={bonus.id}>{bonus.name}</option>
-              )
+                <option key={bonus.id} value={bonus.id}>
+                  {bonus.name}
+                </option>
+              );
             })}
             {/* Step 4: add an option for each accessory */}
           </select>
@@ -41,21 +43,21 @@ export default function CupcakeList() {
       <ul className="cupcake-list" id="cupcake-list">
         {/* Step 2: repeat this block for each cupcake */}
         {cupcakes
-        .filter(cake => {
-          if (filter !== "") {
-            return cake.accessory_id === filter
-          } return cake
-        } )
-        .map(cake => {
-          return (
-        <li className="cupcake-item">
-          <Link to={'/cupcakes/' + cake.id} >
-
-          <Cupcake key={cake.id} cupcake={cake}  />
-          </Link>
-        </li>
-          )
-        })}
+          .filter((cake) => {
+            if (filter !== "") {
+              return cake.accessory_id === filter;
+            }
+            return cake;
+          })
+          .map((cake) => {
+            return (
+              <li className="cupcake-item">
+                <Link to={`/cupcakes/${cake.id}`}>
+                  <Cupcake key={cake.id} cupcake={cake} />
+                </Link>
+              </li>
+            );
+          })}
         {/* end of block */}
       </ul>
     </>
